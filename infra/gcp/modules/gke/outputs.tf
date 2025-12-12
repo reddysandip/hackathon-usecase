b286b5fc30e37cdec1bc6782d40f4db7634bfb46
Sandip Reddy Nukalapati <sandipreddynukalapati@Sandips-Air.Dlink>
2025-12-12 14:55:13 +0530

    up22

--- infra/gcp/modules/gke/outputs.tf
+++ infra/gcp/modules/gke/outputs.tf
@@ -1,19 +1,15 @@
-output "repository_id" {
-  description = "Artifact Registry repository ID"
-  value       = google_artifact_registry_repository.repo.repository_id
+output "cluster_name" {
+  value = google_container_cluster.primary.name
 }
 
-output "repository_location" {
-  description = "Region of the Artifact Registry"
-  value       = google_artifact_registry_repository_repository.repo.location
+output "endpoint" {
+  value = google_container_cluster.primary.endpoint
 }
 
-output "repository_url" {
-  description = "Full Docker repository URL"
-  value       = "${var.region}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.repo.repository_id}"
+output "master_auth" {
+  value = google_container_cluster.primary.master_auth
 }
 
-output "repository_self_link" {
-  description = "Self link for the Artifact Registry repository"
-  value       = google_artifact_registry_repository.repo.self_link
+output "cluster_ca_certificate" {
+  value = google_container_cluster.primary.master_auth[0].cluster_ca_certificate
 }
